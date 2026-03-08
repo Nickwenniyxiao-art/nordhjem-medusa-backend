4d676e9134edc0f0f2b3489a01eadf0ae39130f8
Nickwenniyxiao-art <Nick_wenniyxiao@icloud.com>
2026-03-07 18:42:54 -0800

    fix: export resend notification provider services array

--- src/modules/resend-notification/index.ts
+++ src/modules/resend-notification/index.ts
@@ -1,6 +1,5 @@
-import { Module } from "@medusajs/framework/utils"
 import ResendNotificationProviderService from "./service"
 
-export default Module("resend-notification", {
-  service: ResendNotificationProviderService,
-})
+export default {
+  services: [ResendNotificationProviderService],
+}
